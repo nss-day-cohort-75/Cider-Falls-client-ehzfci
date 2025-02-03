575d5a77b1adb4c8490ccb1c5550cf6518566d3a
soyuz43 <kebekad673@proton.me>
2025-02-03 12:01:23 -0600

    updated footer

--- scripts/Footer.js
+++ scripts/Footer.js
@@ -2,6 +2,8 @@ export const getFooterHTML = () => {
     return `
         <footer>
             <p>&copy; 2025 Cider Falls. All rights reserved.</p>
+            <p>Main Telephone: 615-444-4545. Addess 1212 4th Lane</p>
+            <p>Contact: mailto<fakeemail@example.com></p>
         </footer>
     `;
 };
