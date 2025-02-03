48f2c7adb4085de244612ef1e1d4d87332c6f45d
soyuz43 <kebekad673@proton.me>
2025-02-03 12:59:52 -0600

    fixed 'addess' to 'address'

--- scripts/Footer.js
+++ scripts/Footer.js
@@ -2,7 +2,7 @@ export const getFooterHTML = () => {
     return `
         <footer>
             <p>&copy; 2025 Cider Falls. All rights reserved.</p>
-            <p>Main Telephone: 615-444-4545. Addess 1212 4th Lane</p>
+            <p>Main Telephone: 615-444-4545. Address 1212 4th Lane</p>
             <p>Contact: mailto<fakeemail@example.com></p>
         </footer>
     `;
